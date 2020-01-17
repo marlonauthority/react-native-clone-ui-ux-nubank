c232b0b3bda26094a7534961a5e6b5f4cc41cfef
marlondck <15050029+marlondck@users.noreply.github.com>
2020-01-16 22:19:49 -0400

    tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import {View} from 'react-native';
 
-import {Container} from './styles';
+import {Container, TabsContainer, TabsItem, TabText} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabsItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabsItem>
+        <TabsItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabsItem>
+        <TabsItem>
+          <Icon name="arrow-downward" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabsItem>
+        <TabsItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Transferir</TabText>
+        </TabsItem>
+        <TabsItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear cartão</TabText>
+        </TabsItem>
+      </TabsContainer>
+    </Container>
+  );
 }
