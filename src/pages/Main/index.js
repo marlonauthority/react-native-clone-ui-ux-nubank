cda03a0b48b1d19f297198c85b521b6f6da31019
marlondck <15050029+marlondck@users.noreply.github.com>
2020-01-21 15:25:34 -0400

    layout finished

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import CoinIcon from '~/assets/coin.png';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  Nuconta,
+  TitleNuconta,
+  IconCoin,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Anotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Nuconta>
+              <IconCoin source={CoinIcon} style={{tintColor: '#999'}} />
+              <TitleNuconta>NuConta</TitleNuconta>
+            </Nuconta>
+            <Icon name="visibility-off" size={28} color="#999" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 500.542,75</Description>
+          </CardContent>
+          <CardFooter>
+            <Anotation>
+              Transferência de R$ 20,00 recebida de Marlon hoje às 06:00h.
+            </Anotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -5,4 +5,77 @@ export const Container = styled.View`
   flex: 1;
   background: #8b10ae;
   padding-top: ${getStatusBarHeight(30)}px;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 380px;
+  z-index: 5;
+  margin-bottom: 40px;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+  margin: 0 20px;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const Nuconta = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const IconCoin = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: #fff;
+`;
+
+export const TitleNuconta = styled.Text`
+  padding-left: 10px;
+  font-size: 14px;
+  color: #999;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #eee;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+`;
+
+export const Anotation = styled.Text`
+  font-size: 12px;
+  color: #333;
 `;
